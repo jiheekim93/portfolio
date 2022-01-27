1b2d284234c55932765b22dcecd8dc13229cae5e
Jihee Kim <jiheekim93@gmail.com>
2022-01-27 11:18:21 -0500

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const $myPhoto = () => {
   let $resumeDiv = $('<div>').addClass('resume')
-  let $img1 = $('<img src = "imgs/jihee.jpg">')
+  let $img1 = $('<img src = "jihee.jpg">')
 
   $resumeDiv.append($img1)
+  $('body').append($resumeDiv)
   $('#resume-page').append($resumeDiv)
-  $('body').append('resume')
 }
 
 
