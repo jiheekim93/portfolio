5ca787e0798526aff756e426c80f5fe3a925a42b
Jihee Kim <jiheekim93@gmail.com>
2022-01-28 11:26:45 -0500

    update

--- app.js
+++ app.js
@@ -73,7 +73,10 @@ const $fun = () => {
 
 }
 
-
+const $bio = () => {
+  let $bioText = $('<div>').addClass('bio-text').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+  let $bioPhoto = $('<div>').addClass('bio-photo')
+}
 
 $(() => {
 
