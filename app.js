73b165429cb4f289e8f6909f627127a180c4af18
Jihee Kim <jiheekim93@gmail.com>
2022-01-28 13:30:55 -0500

    update

--- app.js
+++ app.js
@@ -74,15 +74,31 @@ const $fun = () => {
 }
 
 const $bio = () => {
-  let $bioText = $('<div>').addClass('bio-text').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
-  let $bioPhoto = $('<div>').addClass('bio-photo')
+  let $bioDiv = $('<div>').addClass('bio-div')
+  let $bioText = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ').css('color', 'white')
+  let $bioPhoto = $('<img src = "jihee1.jpg">').addClass('jihee-one')
+
+  let $bioDiv2 = $('<div>').addClass('bio-div2')
+  let $bioText1 = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ').css('color', 'white')
+  let $bioPhoto1 = $('<img src = "jihee2.jpg">').addClass('jihee-one')
+
+  $bioDiv.append($bioPhoto)
+  $bioDiv.append($bioText)
+  $('#bio-page').append($bioDiv)
+  $bioDiv2.append($bioText1)
+  $bioDiv2.append($bioPhoto1)
+  $('#bio-page').append($bioDiv2)
+
 }
+
+
+
 
 $(() => {
 
   $myResumePhoto()
   $myResume()
-  // $biography()
   $fun()
+  $bio()
 
 })
