faec61ce65d22034d55aeb40d01bab8541fc6ed7
Jihee Kim <jiheekim93@gmail.com>
2022-01-29 21:26:14 -0500

    update

--- app.js
+++ app.js
@@ -10,41 +10,13 @@ const $myResumePhoto = () => {
 const $myResume = () => {
   let $resumeTextBox = $('<div>').addClass('resume-text')
   let $resumeHeading = $('<h3>').text('My Experience').css('color', 'white')
-  let $resumeText = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.').css('color', 'white')
+  let $resumeText = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ').css('color', 'white')
 
   $('body').append($resumeTextBox)
   $('.resume-text').append($resumeHeading)
   $('#resume-page').append($resumeTextBox)
   $resumeTextBox.append($resumeText)
 }
-
-
-// const $biography = () => {
-//   let currentTextIndex = 0;
-//   let currentTextBox = $('.bio-text-box').children().length - 1
-//
-//   $('.next').on('click', () => {
-//     $('.bio-text-box').children().eq(currentTextIndex).css('display', 'none')
-//
-//     if (currentTextIndex < currentTextBox) {
-//         currentTextIndex ++
-//     } else {
-//       currentTextIndex = 0
-//     }
-//   $('.bio-text-box').children().eq(currentTextIndex).css('display', 'block')
-//   })
-//   $('.previous').on('click',() => {
-//     $('.bio-text-box').children().eq(currentTextIndex).css('display', 'none')
-//
-//     if (currentTextIndex > 0) {
-//       currentTextIndex --
-//     } else {
-//       currentTextIndex = currentTextBox
-//     }
-//     $('.bio-text-box').children().eq(currentTextIndex).css('display', 'block')
-//   })
-//
-// }
 
 const $fun = () => {
   let currentImgIndex = 0;
@@ -99,16 +71,32 @@ const $bio = () => {
 
 }
 
+const $project = () => {
+  $projectDiv = $('<div>').addClass('project-div').appendTo('body')
+  $projectCon1 = $('<div>').addClass('project-contaier1').appendTo('.project-div')
+  $projectCon2 = $('<div>').addClass('project-contaier2').appendTo('.project-div')
+  $projectCon3 = $('<div>').addClass('project-contaier3').appendTo('.project-div')
 
+  $project1 = $('<img src = "color-picker.jpg">').addClass('color-picker').appendTo('.project-contaier1')
+  $project2 = $('<img src = "users-data-base.jpg">').addClass('data-base').appendTo('.project-contaier2')
+  $project3 = $('<img src = "field-lights-theme.jpg">').addClass('field-lights').appendTo('.project-contaier3')
+
+  $projectText1 = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco').appendTo('.project-contaier1')
+  $projectText2 = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco').appendTo('.project-contaier2')
+  $projectText3 = $('<p>').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco').appendTo('.project-contaier3')
+
+  $('#project-page').append($projectDiv)
+
+
+}
 
 
 
 $(() => {
-
   $myResumePhoto()
   $myResume()
+  $project()
   $fun()
   $funText()
   $bio()
-
 })
